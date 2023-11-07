20419423da4c387583c800f4aa57f2a5608228b1
Bobbi Towers <btowers793@gmail.com>
2023-11-07 04:45:40 -0800

    enlarge canvas to 752x720

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { Controller, buttonDown, buttonUp } from "./src/controller";
 var canvas = document.getElementById("frameCanvas")
 
 var ctx = canvas.getContext("2d");
-var imageData = ctx.getImageData(0,0,256,240);
+var imageData = ctx.getImageData(0, 0, 256, 240);
 var frameBuffer = new ArrayBuffer(imageData.data.length);
 var frameBuffer8 = new Uint8ClampedArray(frameBuffer);
 var frameBuffer32 = new Uint32Array(frameBuffer);
@@ -21,9 +21,9 @@ audioprocessor.connect(audio.destination);
 audioprocessor.onaudioprocess = audioEvent => {
 
   // Ensure that we've buffered enough samples
-  if(leftSamples.length > currentSample + 2048){
-    for(var i = 0; i < 2048; i++){
-    
+  if (leftSamples.length > currentSample + 2048) {
+    for (var i = 0; i < 2048; i++) {
+
       // Output (play) the buffers
       audioEvent.outputBuffer.getChannelData(0)[i] = leftSamples[currentSample];
       //audioEvent.outputBuffer.getChannelData(1)[i] = rightSamples[currentSample];
@@ -40,14 +40,15 @@ var currentSample = 0;
 var interval;
 var start = file => {
 
+
   // Initialize JSNES
   NES.init({
-    
+
     // Display each new frame on the canvas
-    onFrame: function(frameBuffer){
+    onFrame: function (frameBuffer) {
       var i = 0;
-      for(var x = 0; x < 256; ++x){
-        for(var y = 0; y < 240; ++y){
+      for (var x = 0; x < 256; ++x) {
+        for (var y = 0; y < 240; ++y) {
           i = y * 256 + x;
           frameBuffer32[i] = 0xff000000 | frameBuffer[i];
         }
@@ -55,9 +56,9 @@ var start = file => {
       imageData.data.set(frameBuffer8);
       ctx.putImageData(imageData, 0, 0);
     },
-    
+
     // Add new audio samples to the Audio buffers
-    onAudioSample: function(left){
+    onAudioSample: function (left) {
       leftSamples.push(left);
       //rightSamples.push(right);
     }
@@ -65,10 +66,10 @@ var start = file => {
 
   // Reset emulator
   NES.reset();
-  
+
   // Send ROM to emulator
   NES.load_rom(file);
-  
+
   // First 10 frames are garbage
   NES.frame();
   NES.frame();
@@ -84,30 +85,30 @@ var start = file => {
   // 60 fps loop
   clearInterval(interval);
   interval = setInterval(NES.frame, 17);
-  
+
   // Controller #1 keys listeners
   onkeydown = e => {
     e.preventDefault()
-    if(e.keyCode == 69) buttonDown(1, Controller.BUTTON_LEFT);
-    else if(e.keyCode == 82) buttonDown(1, Controller.BUTTON_UP);
-    else if(e.keyCode == 68) buttonDown(1, Controller.BUTTON_RIGHT);
-    else if(e.keyCode == 78) buttonDown(1, Controller.BUTTON_DOWN);
-    else if(e.keyCode == 32) buttonDown(1, Controller.BUTTON_A); // X
-    else if(e.keyCode == 72) buttonDown(1, Controller.BUTTON_B); // C
-    else if(e.keyCode == 27) buttonDown(1, Controller.BUTTON_SELECT);
-    else if(e.keyCode == 13) buttonDown(1, Controller.BUTTON_START);
+    if (e.keyCode == 69) buttonDown(1, Controller.BUTTON_LEFT);
+    else if (e.keyCode == 82) buttonDown(1, Controller.BUTTON_UP);
+    else if (e.keyCode == 68) buttonDown(1, Controller.BUTTON_RIGHT);
+    else if (e.keyCode == 78) buttonDown(1, Controller.BUTTON_DOWN);
+    else if (e.keyCode == 32) buttonDown(1, Controller.BUTTON_A); // X
+    else if (e.keyCode == 72) buttonDown(1, Controller.BUTTON_B); // C
+    else if (e.keyCode == 27) buttonDown(1, Controller.BUTTON_SELECT);
+    else if (e.keyCode == 13) buttonDown(1, Controller.BUTTON_START);
   }
 
   onkeyup = e => {
     e.preventDefault()
-    if(e.keyCode == 69) buttonUp(1, Controller.BUTTON_LEFT);
-    else if(e.keyCode == 82) buttonUp(1, Controller.BUTTON_UP);
-    else if(e.keyCode == 68) buttonUp(1, Controller.BUTTON_RIGHT);
-    else if(e.keyCode == 78) buttonUp(1, Controller.BUTTON_DOWN);
-    else if(e.keyCode == 32) buttonUp(1, Controller.BUTTON_A); // X
-    else if(e.keyCode == 72) buttonUp(1, Controller.BUTTON_B); // C
-    else if(e.keyCode == 27) buttonUp(1, Controller.BUTTON_SELECT);
-    else if(e.keyCode == 13) buttonUp(1, Controller.BUTTON_START);
+    if (e.keyCode == 69) buttonUp(1, Controller.BUTTON_LEFT);
+    else if (e.keyCode == 82) buttonUp(1, Controller.BUTTON_UP);
+    else if (e.keyCode == 68) buttonUp(1, Controller.BUTTON_RIGHT);
+    else if (e.keyCode == 78) buttonUp(1, Controller.BUTTON_DOWN);
+    else if (e.keyCode == 32) buttonUp(1, Controller.BUTTON_A); // X
+    else if (e.keyCode == 72) buttonUp(1, Controller.BUTTON_B); // C
+    else if (e.keyCode == 27) buttonUp(1, Controller.BUTTON_SELECT);
+    else if (e.keyCode == 13) buttonUp(1, Controller.BUTTON_START);
   }
 };
 
@@ -127,7 +128,7 @@ var play = path => {
   file.open('GET', path);
   file.overrideMimeType("text/plain; charset=x-user-defined");
   file.send();
-  file.onload = function(){
+  file.onload = function () {
     start(file.responseText);
   }
 }
